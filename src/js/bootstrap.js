f7855c6b77bdf9dd0b4af89f58dfefc8e5093c98
DManali <manali76@gmail.com>
2016-04-21 22:04:00 -0400

    revert

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -4,7 +4,7 @@
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
-if (typeo jQuery === 'undefined') {
+if (typeos jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
