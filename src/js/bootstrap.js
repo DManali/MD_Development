52a1395e8224c3dff1e13bd90ee8e9e4d399fcd0
DManali <manali76@gmail.com>
2016-05-10 15:08:19 -0400

    lab reports and iamging tab changes

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -4,7 +4,7 @@
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
-if (typeos jQuery === 'undefined') {
+if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
